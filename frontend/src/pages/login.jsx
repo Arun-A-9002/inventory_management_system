a05cba9551ce2ecd02dba0bfe35dc793e9cd6f8a
Arun-A-9002 <arun@nutryah.com>
2025-12-09 11:24:22 +0530

    login to dashboard

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -9,7 +9,7 @@ export default function Login() {
   const [otpSent, setOtpSent] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  // TIMER (5 minutes)
+  // TIMER (5   minutes)
   const [timer, setTimer] = useState(300);
 
   // Start countdown when OTP is sent
